274f59f4bf782a42b2b09f84837433a456156c51
pietrafn <71992173+Pietrafn@users.noreply.github.com>
2022-09-25 22:23:18 -0300

    Create Javascrip.js

---
+++ Javascrip.js
@@ -0,0 +1,22 @@
+var filmes = [
+  {
+    nome: "Pixels",
+    img:
+      "https://th.bing.com/th/id/OIP.CfMxP5OXwVIZvCUbqCrwDwHaK-?w=121&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+    link: "https://www.youtube.com/watch?v=h0pMaAMFDIY",
+    sinopse:
+      "Quando seres intergalácticos interpretam um arquivo em vídeo com imagens de jogos de arcade clássicos como uma declaração de guerra contra eles, eles atacam a Terra usando esses jogos como modelos para suas várias ofensivas."
+  },
+  {
+    nome: "Bob Esponja - O Filme",
+    img:
+      "https://th.bing.com/th/id/OIP.AjgjzXCg3qCoTqI2E3BYQwHaKc?pid=ImgDet&w=208&h=294&c=7&dpr=1,25",
+    link: "https://www.youtube.com/watch?v=6SiWdN7Llno",
+    sinopse:
+      "Mergulhe nesta comédia de aventura, que é maior, melhor e mais envolvente que todas as outras: o filme de estréia desta sensação submarina, que é Bob Esponja! Um grande problema borbulha na Fenda do Bikini. A coroa do Rei Netuno desapareceu e o Sr. Sirigueijo foi acusado de roubá-la!"
+  }
+];
+
+var elementolistafilmes = document.getElementById("listaFilmes");
+
+mostrarfilmes();
